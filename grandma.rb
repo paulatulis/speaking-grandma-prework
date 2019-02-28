541c2f254f9c81d88c5579e61f405c623855aea0
Paula Tulis <tulis.paula@gmail.com>
2019-02-28 01:24:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,6 @@
 # Write a speak_to_grandma method.
-phrase = "I LOVE YOU GRANDMA"
-def speak_to_grandma
-  if phrase == "I LOVE YOU GRANDMA"
+speak_to_grandma = "I LOVE YOU GRANDMA"
+if speak_to_grandma == "I LOVE YOU GRANDMA"
     puts "I LOVE YOU TOO PUMPKIN!"
 end
 
