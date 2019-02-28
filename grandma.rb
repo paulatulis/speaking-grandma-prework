436a5896679e0b289fec71dc4d174de049427626
Paula Tulis <tulis.paula@gmail.com>
2019-02-28 01:54:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,6 +5,8 @@ def speak_to_grandma(phrase)
   phrase = "I LOVE YOU GRANDMA!"
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elseif phrase == "Hi Nana, how are you?"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
