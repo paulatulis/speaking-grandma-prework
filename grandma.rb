b916b0cd1b850eaac30119a84a3cd56c90c57823
Paula Tulis <tulis.paula@gmail.com>
2019-02-28 02:21:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -10,7 +10,7 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP, SONNY!"
   elseif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  elseif phrase
+  else
     return "NO, NOT SINCE 1938!"
   end
 end
