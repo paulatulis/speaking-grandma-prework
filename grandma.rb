18eab2de4ee20731298dd57c94b7dca0de114a34
Paula Tulis <tulis.paula@gmail.com>
2019-02-28 01:40:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 
 phrase = "Hi!"
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
+  if phrase = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO, PUMPKIN!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
